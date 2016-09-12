1fa955e0eee2e141bea94a523064acef4b3d3e8f
harish <hb.harishbalaji@gmail.com>
2016-09-12 10:29:48 -0400

    Address Book basic

---
+++ src/Address.java
@@ -0,0 +1,103 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Address {
+	
+	private String name,addr,city,state,zip,phone,email;
+	
+	private boolean isEmailValid(String email){  
+        boolean isValid = false;  
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";  
+        CharSequence inputStr = email;  
+        //Make the comparison case-insensitive.  
+        Pattern pattern = Pattern.compile(expression,Pattern.CASE_INSENSITIVE);  
+        Matcher matcher = pattern.matcher(inputStr);  
+        if(matcher.matches()){  
+            isValid = true;  
+        }  
+        return isValid;  
+    } 
+    
+    private boolean isPhoneNumberValid(String phoneNumber){  
+        boolean isValid = false;  
+
+        String expression = "^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$";  
+        CharSequence inputStr = phoneNumber;  
+        Pattern pattern = Pattern.compile(expression);  
+        Matcher matcher = pattern.matcher(inputStr);  
+        if(matcher.matches()){  
+            isValid = true;  
+        }  
+        return isValid;  
+    }
+	
+	public Address()
+	{
+		System.out.println("\nEnter Name, Address, City, State, Zip, Phone and email details :  ");
+	}
+	
+	public void setName(String a)
+	{
+		name = a;
+	}
+	
+	public void setAddress(String a)
+	{
+		addr = a;
+	}
+	public void setCity(String a)
+	{
+		city = a;
+	}
+	public void setState(String a)
+	{
+		state = a;
+	}
+	public void setZip(String a)
+	{
+		zip = a;
+	}
+	public void setPhone(String a)
+	{
+		if (isPhoneNumberValid(a)) 
+			phone = a;
+		else phone = "invalid";
+	}
+	public void setEmail(String a)
+	{
+		if (isEmailValid(a)) 
+			email = a;
+		else email = "invalid";
+	}
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getAddress()
+	{
+		return addr;
+	}
+	public String getCity()
+	{
+		return city;
+	}
+	public String getState()
+	{
+		return state;
+	}
+	public String getZip()
+	{
+		return zip ;
+	}
+	public String getPhone()
+	{
+		return phone;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+
+}
---
+++ src/AddressApp.java
@@ -0,0 +1,45 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+
+public class AddressApp {
+	
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		Address a = new Address();
+		String x = sc.next();
+		a.setName(x);
+		x = sc.next();
+		a.setAddress(x);
+		x = sc.next();
+		a.setCity(x);
+		x = sc.next();
+		a.setState(x);
+		x = sc.next();
+		a.setZip(x);
+		do 
+		{		x = sc.next();
+				a.setPhone(x);
+				if(a.getPhone().equals("invalid"))
+					System.out.println("Phone number is invalid - enter in  \"(XXX)-XXX-XXXX\" format");
+		}while(a.getPhone().equals("invalid"));
+		
+		do 
+		{
+				x = sc.next();
+				a.setEmail(x);
+				if(a.getEmail().equals("invalid"))
+					System.out.println("Email is invalid - enter in \"name@domain.com\" format ");
+		}while(a.getEmail().equals("invalid"));
+		
+		System.out.println("\n The deatils of the address are as follows : \n");
+		System.out.println(a.getName() + " " + a.getAddress() + " " + a.getCity() + 
+				" " + a.getState() + " " + a.getZip() + " " + a.getPhone() + " " + a.getEmail());
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
